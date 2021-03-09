1447d5eafbca74c385567dd94d07b0ec5a4abf2b
Stephan <sgriesel@gmail.com>
2021-03-09 07:03:45 +0100

    test page query & static query data

--- src/components/Home/Treatments.js
+++ src/components/Home/Treatments.js
@@ -1,41 +1,41 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 
+const getData = graphql`
+{
+  items: allContentfulHappyItem {
+    edges {
+      node {
+        id
+        title
+        price
+        category
+        image {
+          fixed(width: 50, height: 50) {
+            src
+          }
+        }
+        description
+      }
+    }
+  }
+}
+`
+
 const Treatments = () => {
-    return (
-        <>
-            <Wrapper>
-                data here
-            </Wrapper>
-        </>
-    )
+  const data = useStaticQuery(getData)
+  console.log("static query graphql data", data)
+  return (
+    <>
+      <Wrapper>
+        {/* test: {data.items.edges[0].node.title} */}
+      </Wrapper>
+    </>
+  )
 }
 
 const Wrapper = styled.main`
 min-height:100vh;
 `
-
-export const query = graphql`
-  {
-    items: allContentfulHappyItem {
-      edges {
-        node {
-          id
-          title
-          price
-          category
-          image {
-            fixed(width: 50, height: 50) {
-              src
-            }
-          }
-          description
-        }
-      }
-    }
-  }
-`
-
-
-
 export default Treatments
--- src/pages/treatments.js
+++ src/pages/treatments.js
@@ -1,55 +1,27 @@
 import React from "react"
 import { graphql } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-// import BackgroundSection from '../components/Globals/BackgroundSection';
-
-// import Info from '../components/Home/Info'
-
-import Menu from '../components/Home/Menu'
 import Treatments from '../components/Home/Treatments'
 
+const TreatmentsPage = ({ data }) => {
+  console.log("page query data test", data);
+  return (
+    <Layout>
+      <SEO title="Treatments" />
+      <p>{data.items.nodes[0].title}</p>
+      <Treatments />
+    </Layout >
+  )
+}
 
-const TreatmentsPage = ({ data }) => (
-  <Layout>
-    <SEO title="Treatments" />
-    {/* <BackgroundSection
-      img={data.img.childImageSharp.fluid}
-      title="Treatments"
-      styleClass="about-background"
-    /> */}
-    <Menu items={data.menu} />
-    <Treatments />
-  </Layout >
-);
-
-export const query = graphql`
-{
-  img:file(relativePath:{
-    eq:"default-bg.jpg"
-  }){
-    childImageSharp{
-      fluid(quality:100) {
-        ...GatsbyImageSharpFluid_tracedSVG
-      }
-    }
-  }
-  menu:allContentfulHappyItem{
-    edges{
-      node{
-        id
-        title
-        price
-        category
-        image {
-          fixed(width:50,height:50){
-            ...GatsbyContentfulFixed
-          }
-        }
-        description 
-      }
+export const data = graphql`
+query MyQuery {
+  items: allContentfulHappyItem {
+    nodes {
+      title
+      category
+      price
     }
   }
 }
