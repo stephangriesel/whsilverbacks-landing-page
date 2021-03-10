cef02126dc009c5941cd4e8876f0b8950ec25f98
Stephan <sgriesel@gmail.com>
2021-03-10 07:25:39 +0100

    update query & map items

--- src/pages/treatments.js
+++ src/pages/treatments.js
@@ -2,26 +2,28 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Treatments from '../components/Home/Treatments'
 
 const TreatmentsPage = ({ data }) => {
   console.log("page query data test", data);
+  const { items: { info } } = data
   return (
     <Layout>
       <SEO title="Treatments" />
-      <p>{data.items.nodes[0].title}</p>
-      <Treatments />
+      <>
+        <p>{info.map(item => <div>{item.title}: <strong>R{item.price}</strong></div>)}</p>
+      </>
     </Layout >
   )
 }
 
 export const data = graphql`
 query MyQuery {
-  items: allContentfulHappyItem {
-    nodes {
+  items: allContentfulHappyItem(sort: {fields: title, order: ASC}) {
+    info:nodes {
       title
       category
       price
+      id
     }
   }
 }
