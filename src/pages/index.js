e9ce5ea7e6ad8dd14db0a24c8c019a5c98e91ba1
Stephan <sgriesel@gmail.com>
2021-12-12 08:29:48 +0100

    test parallax

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,25 +6,52 @@ import SEO from "../components/seo"
 
 import BackgroundSection from "../components/Globals/BackgroundSection"
 
+import { Parallax, Background } from 'react-parallax';
+
+
 // import Info from '../components/Home/Info'
 import LatestProducts from "../components/Home/ProductHighlights"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <BackgroundSection
+    {/* <BackgroundSection
       img={data.img.childImageSharp.fluid}
       title="ChipChop"
       styleClass="default-background"
-    />
-    {/* <Info /> */}
+    /> */}
+    <Parallax
+      blur={{ min: -5, max: 5 }}
+      bgImage={require('../images/default.jpg')}
+      bgImageAlt="the dog"
+      strength={-100}
+      style={
+        {
+          marginTop: '20px',
+          marginBottom: '20px',
+        }
+      }
+    >
+      {/* Blur transition from min to max */}
+      <div style={{ height: '700px', padding: '20px' }} />
+    </Parallax>
+    <Parallax
+      blur={{ min: -15, max: 15 }}
+      bgImage={require('../images/main2.jpg')}
+      bgImageAlt="the dog"
+      strength={-200}
+    >
+      Blur transition from min to max
+      <div style={{ height: '400px' }} />
+    </Parallax>
     <LatestProducts />
+    {/* <Info /> */}
   </Layout>
 )
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "default-bg.jpeg" }) {
+    img: file(relativePath: { eq: "default.jpg" }) {
       childImageSharp {
         fluid(quality: 100) {
           ...GatsbyImageSharpFluid_tracedSVG
