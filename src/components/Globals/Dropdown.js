4d578e1e5931bc9cd92be46b548baa0cfa05c1f8
Stephan <sgriesel@gmail.com>
2022-01-21 06:37:49 +0100

    start dropdown

---
+++ src/components/Globals/Dropdown.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Link } from "gatsby"
+import { menuItems } from "./"
+import "../css/dropdown.css"
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+
+`
--- src/components/Globals/Navigation.js
+++ src/components/Globals/Navigation.js
@@ -50,4 +50,8 @@ function Navigation(){
   )
 }
 
+const Wrapper = styled.div`
+
+`
+
 export default Navigation;
