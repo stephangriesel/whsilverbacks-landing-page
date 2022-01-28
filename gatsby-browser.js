e204f8c9da27122ad5a9447c3efea0ebfbf85432
Stephan <sgriesel@gmail.com>
2022-01-28 08:29:43 +0100

    scroll view logic

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,15 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+// in gastby-browser.js
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  const { pathname } = location
+  // list of routes for the scroll-to-top-hook
+  const scrollToTopRoutes = [`/privacy-policy`, `/page-2`]
+  // if the new route is part of the list above, scroll to top (0, 0)
+  if (scrollToTopRoutes.indexOf(pathname) !== -1) {
+    window.scrollTo(0, 0)
+  }
 
-// You can delete this file if you're not using it
+  return false
+}
