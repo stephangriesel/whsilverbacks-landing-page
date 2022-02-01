ae726690c3e96ab3f0ac3f5035daaff8965d2f9e
Stephan <sgriesel@gmail.com>
2022-02-01 07:32:29 +0100

    prepare reviews for local state

--- src/pages/review.js
+++ src/pages/review.js
@@ -1,18 +1,19 @@
-import React from "react"
+import React,  { useState, useEffect } from "react"
 import Layout from "../components/layout"
 import Footer from "../components/Globals/Footer"
 import SEO from "../components/seo"
 
 import AddReview from "../components/Reviews/AddReview"
+import Reviews from "../components/Reviews/Reviews"
+
 import styled from 'styled-components';
 
 const review = ({ data }) => (
   <Layout>
     <SEO title="Why ChipChop" />
     <Wrapper>
-      <AddReview />
-      {/* <ProductReview onChange={log} /> */}
-      </Wrapper>
+      <AddReview />  
+    </Wrapper>
     <Footer />
   </Layout>
 )
@@ -25,6 +26,9 @@ align-items:center;
 padding:10px;
 margin:15px;
 
+`
+
+const Header = styled.div`
 `
 
 export default review
