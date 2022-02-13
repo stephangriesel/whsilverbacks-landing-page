5ef923fcc7e5cbfe28db3f03edd97e6634dd6af0
Stephan <sgriesel@gmail.com>
2022-02-13 10:48:17 +0100

    add shop

--- src/components/Home/Products.js
+++ src/components/Home/Products.js
@@ -1,47 +1,47 @@
-// import React from 'react'
-// import Product from './Product'
-// // import Title from '../Globals/Title'
-// import { StaticQuery, graphql } from 'gatsby'
+import React from 'react'
+import Product from './Product'
+// import Title from '../Globals/Title'
+import { StaticQuery, graphql } from 'gatsby'
 
-// const getProducts = graphql`
-// {
-//     products:allContentfulProduct{
-//       edges{
-//         node{
-//           id
-//           title
-//           price
-//           image{
-//             fluid(maxHeight:450){
-//                 src
-//               ...GatsbyContentfulFluid_tracedSVG
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+const getProducts = graphql`
+{
+    products:allContentfulProduct{
+      edges{
+        node{
+          id
+          title
+          price
+          image{
+            fluid(maxHeight:450){
+                src
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
-// export default function Products() {
-//   return (
-//     <StaticQuery query={getProducts} render={data => {
-//       return (
-//         <section className="fade-in">
-//           <section className="py-5">
-//             <div className="container">
-//               {/* <Title title="ChipChop Products" /> */}
-//               <div className="row">
-//                 {data.products.edges.map(({ node: product }) => {
-//                   return <Product key={product.id} product={product} />
-//                 })}
-//               </div>
-//             </div>
-//           </section>
-//         </section>
+export default function Products() {
+  return (
+    <StaticQuery query={getProducts} render={data => {
+      return (
+        <section className="fade-in">
+          <section className="py-5">
+            <div className="container">
+              {/* <Title title="ChipChop Products" /> */}
+              <div className="row">
+                {data.products.edges.map(({ node: product }) => {
+                  return <Product key={product.id} product={product} />
+                })}
+              </div>
+            </div>
+          </section>
+        </section>
 
-//       )
-//     }} />
-//   );
-// }
+      )
+    }} />
+  );
+}
 
