47bbb8085a623ab67f7ef69f90f8e254135883bb
Stephan <sgriesel@gmail.com>
2022-02-21 18:09:36 +0100

    remove temp component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,170 @@
 import React from "react"
 import { graphql } from "gatsby"
+import styled from "styled-components"
+import SEO from "../components/seo";
+import Logo from "../images/logo.png"
 
-import Layout from "../components/layout"
-// import Footer from "../components/Globals/Footer"
-// import SEO from "../components/seo"
-import Temp from "../components/temp"
-
-// import BackgroundSection from "../components/Globals/BackgroundSection"
-
-// import { Parallax } from 'react-parallax';
-
-
-// import Info from '../components/Home/Info'
-// import LatestProducts from "../components/Home/ProductHighlights"
-
-const IndexPage = ({ data }) => (
-<>
-
-    <Temp />
-
-    </>
+const IndexPage = ({ }) => (
+  <Wrapper>
+    <SEO title="Coming Soon" />
+    <div className="brand">
+      <img src={Logo} alt="" srcset="" />
+    </div>
+    <div>
+      <p className="custom-font sub-heading">
+        stretch tents | event construction | temporary structures
+      </p>
+      <p className="temp">
+        <span className="custom-font">under construction</span>
+      </p>
+      <div className="loading">
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+      </div>
+      <p className="custom-font temp2">
+        Get in touch
+      </p>
+      <p>
+        <a className="custom-font temp2" href="mailto:info@silverbacks.co.uk">info@silverbacks.co.uk</a>
+      </p>
+      <p className="custom-font temp3">
+        07956 102133 <span className="inbetween-text custom-font">OR</span> 07446 984468
+      </p>
+      <div className="footer-temp custom-font">
+        &copy;
+        <span id="copyright">
+          <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear())) </script>
+        </span>
+        {" "}WH GROUP LTD
+      </div>
+    </div>
+  </Wrapper>
 )
+
+const Wrapper = styled.div`
+a {
+  color:#FFF !important;
+}
+display:grid;
+justify-content:center;
+align-content:center;
+height:100vh;
+background-image: linear-gradient(
+  190deg,
+  hsl(33deg 13% 31%) 0%,
+  hsl(77deg 8% 27%) 0%,
+  hsl(151deg 9% 25%) 2%,
+  hsl(185deg 12% 22%) 6%,
+  hsl(192deg 11% 22%) 15%,
+  hsl(168deg 7% 25%) 29%,
+  hsl(117deg 3% 29%) 47%,
+  hsl(52deg 3% 32%) 64%,
+  hsl(70deg 3% 31%) 77%,
+  hsl(141deg 4% 28%) 87%,
+  hsl(177deg 8% 24%) 95%,
+  hsl(198deg 12% 21%) 100%
+);
+margin:0;
+padding:0;
+
+p {
+  color:#FFF;
+  text-align:center;
+}
+
+.inbetween-text {
+  font-size:0.8em;
+}
+
+.sub-heading {
+  font-weight:900;
+  text-transform:uppercase;
+  letter-spacing:0.2em;
+  margin:2em 0;
+}
+
+.temp {
+  font-size:2.0em;
+  text-transform:uppercase;
+  font-weight:900;
+  text-shadow: 2px 2px 16px rgba(255, 255, 255, 1);
+  margin:1.5em 0;
+  letter-spacing:0.1em;
+  width:100%;
+}
+
+.temp2 {
+  margin:0.5em;
+  font-size:1.2em;
+  letter-spacing:0.1em;
+  text-transform:uppercase;
+  font-weight:600;
+}
+
+.temp3 {
+  margin:0.5em;
+  font-size:1.1em;
+  letter-spacing:0.1em;
+}
+
+.brand {
+  display:flex;
+  justify-content:center;
+  img {
+  width:100%;
+  max-width:30em;
+  margin-top:2em;
+}
+}
+
+.footer-temp {
+  text-align:center;
+  margin-top:2em;
+  color:#FFF;
+}
+
+/* Loading Animation */
+
+.loading {
+  display:flex;
+  justify-content:center;
+}
+
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #fff transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+`
 
 export const query = graphql`
   {
